a9c428733cc2b69b24a3bcb92ecb8b7604039075
ykamikawa <ytkmkw@gmail.com>
2017-11-29 00:56:00 +0900

    first commit

---
+++ predict.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import cv2
+import numpy as np
+from tqdm import tqdm
+from preprocessing import parse_annotation
+from utils import draw_boxes
+from frontend import YOLO
+import json
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+
+argparser = argparse.ArgumentParser(
+    description='Train and validate YOLO_v2 model on any dataset')
+
+argparser.add_argument(
+    '-c',
+    '--conf',
+    help='path to configuration file')
+
+argparser.add_argument(
+    '-w',
+    '--weights',
+    help='path to pretrained weights')
+
+argparser.add_argument(
+    '-i',
+    '--input',
+    help='path to an image or an video (mp4 format)')
+
+argparser.add_argument(
+    '-o',
+    '--output',
+    default="./outputs/",
+    help='path to output dirctory (mp4 format)')
+
+def _main_(args):
+
+    config_path  = args.conf
+    weights_path = args.weights
+    image_path   = args.input
+
+    with open(config_path) as config_buffer:
+        config = json.load(config_buffer)
+
+    # build model
+    yolo = YOLO(architecture        = config['model']['architecture'],
+                input_size          = config['model']['input_size'],
+                labels              = config['model']['labels'],
+                max_box_per_image   = config['model']['max_box_per_image'],
+                anchors             = config['model']['anchors'])
+
+    # load pretrained model
+    print(weights_path)
+    yolo.load_weights(weights_path)
+
+    # predict bounding box
+    if image_path[-4:] == '.mp4':
+        input_file = os.path.basename(args.input)
+        video_out = args.output+input_file[:-4]+"_demo.mp4"
+
+        video_reader = cv2.VideoCapture(image_path)
+
+        nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+        video_writer = cv2.VideoWriter(video_out,
+                               cv2.VideoWriter_fourcc(*'MPEG'),
+                               30.0,
+                               (frame_w, frame_h))
+
+        for i in tqdm(range(nb_frames)):
+            _, image = video_reader.read()
+
+            boxes = yolo.predict(image)
+            image = draw_boxes(image, boxes, config['model']['labels'])
+
+            video_writer.write(np.uint8(image))
+
+        video_reader.release()
+        video_writer.release()
+    else:
+        image = cv2.imread(image_path)
+        boxes = yolo.predict(image)
+        image = draw_boxes(image, boxes, config['model']['labels'])
+
+        print(len(boxes), 'boxes are found')
+
+        input_file = os.path.basename(args.input)
+        cv2.imwrite(args.output+input_file[:-4]+"_demo.png")
+
+if __name__ == '__main__':
+    args = argparser.parse_args()
+    _main_(args)
---
+++ train.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+import numpy as np
+from preprocessing import parse_annotation
+from frontend import YOLO
+import json
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
+argparser = argparse.ArgumentParser(
+    description='Train and validate YOLO_v2 model on any dataset')
+
+argparser.add_argument(
+    '-c',
+    '--conf',
+    help='path to configuration file')
+
+def _main_(args):
+
+    config_path = args.conf
+
+    with open(config_path) as config_buffer:
+        config = json.loads(config_buffer.read())
+
+    # parse annotations of the training set
+    train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'],
+                                                config['train']['train_image_folder'],
+                                                config['model']['labels'])
+
+    # parse annotations of the validation set, if any, otherwise split the training set
+    if os.path.exists(config['valid']['valid_annot_folder']):
+        valid_imgs, valid_labels = parse_annotation(config['valid']['valid_annot_folder'],
+                                                    config['valid']['valid_image_folder'],
+                                                    config['model']['labels'])
+    else:
+        train_valid_split = int(0.8*len(train_imgs))
+        np.random.shuffle(train_imgs)
+
+        valid_imgs = train_imgs[train_valid_split:]
+        train_imgs = train_imgs[:train_valid_split]
+
+    # detected labels
+    overlap_labels = set(config['model']['labels']).intersection(set(train_labels.keys()))
+
+    print ('Seen labels:\t', train_labels)
+    print ('Given labels:\t', config['model']['labels'])
+    print ('Overlap labels:\t', overlap_labels)
+
+    if len(overlap_labels) < len(config['model']['labels']):
+        print ('Some labels have no images! Please revise the list of labels in the config.json file!')
+        return
+
+    # construct models
+    yolo = YOLO(architecture        = config['model']['architecture'],
+                input_size          = config['model']['input_size'],
+                labels              = config['model']['labels'],
+                max_box_per_image   = config['model']['max_box_per_image'],
+                anchors             = config['model']['anchors'])
+
+    # load pretrained models
+    if os.path.exists(config['train']['pretrained_weights']):
+        print ("Loading pre-trained weights in", config['train']['pretrained_weights'])
+        yolo.load_weights(config['train']['pretrained_weights'])
+
+    # start trianing
+    yolo.train(train_imgs         = train_imgs,
+               valid_imgs         = valid_imgs,
+               train_times        = config['train']['train_times'],
+               valid_times        = config['valid']['valid_times'],
+               nb_epoch           = config['train']['nb_epoch'],
+               learning_rate      = config['train']['learning_rate'],
+               batch_size         = config['train']['batch_size'],
+               warmup_bs          = config['train']['warmup_batches'],
+               object_scale       = config['train']['object_scale'],
+               no_object_scale    = config['train']['no_object_scale'],
+               coord_scale        = config['train']['coord_scale'],
+               class_scale        = config['train']['class_scale'],
+               saved_weights_name = config['train']['saved_weights_name'],
+               debug              = config['train']['debug'])
+
+if __name__ == '__main__':
+    args = argparser.parse_args()
+    _main_(args)
